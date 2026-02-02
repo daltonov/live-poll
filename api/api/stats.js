7900127d1405378094587b7d529f928fa863c935
daltonov <krllsmorodin@gmail.com>
2026-02-02 19:23:43 +0500

    Delete api/api directory

--- api/api/stats.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const votes = globalThis.votes || { A: 0, B: 0, C: 0 };
-
-  res.json({
-    labels: ["A", "B", "C"],
-    values: [votes.A, votes.B, votes.C]
-  });
-}
