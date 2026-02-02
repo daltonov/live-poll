492a343f9c8bf80c045aaf9e8128490d65cc8feb
daltonov <krllsmorodin@gmail.com>
2026-02-03 00:14:50 +0500

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -2,20 +2,8 @@ import { kv } from '@vercel/kv'
 
 export default async function handler(req, res) {
   try {
-    console.log('KV ENV:', {
-      url: process.env.KV_REST_API_URL?.slice(0, 30),
-      token: !!process.env.KV_REST_API_TOKEN,
-    })
-
-    const allKeys = await kv.keys('*')
-    console.log('KV KEYS:', allKeys)
-
-    const data = {}
-    for (const key of allKeys) {
-      data[key] = await kv.get(key)
-    }
-
-    res.status(200).json(data)
+    const votes = (await kv.get('votes')) || {}
+    res.status(200).json(votes)
   } catch (e) {
     console.error('STATS ERROR', e)
     res.status(500).json({ error: e.message })
