e67d56a9a1608719e359c49da136fd5954aedd5e
daltonov <krllsmorodin@gmail.com>
2026-02-02 21:16:07 +0500

    Update yandex-form.js

--- api/yandex-form.js
+++ api/yandex-form.js
@@ -1,11 +1,13 @@
+// Храним голоса в памяти (для ивента этого достаточно)
 globalThis.votes = globalThis.votes || {};
 
 export default function handler(req, res) {
-  const answers = req.body?.answers;
+  // Яндекс Форма пришлёт параметр answer
+  const variantId = req.body?.answer;
 
-  if (answers) {
-    const value = Object.values(answers)[0];
-    globalThis.votes[value] = (globalThis.votes[value] || 0) + 1;
+  if (variantId) {
+    globalThis.votes[variantId] =
+      (globalThis.votes[variantId] || 0) + 1;
   }
 
   res.status(200).json({ ok: true });
